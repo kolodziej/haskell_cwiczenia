93351ca5b9946729ec31b810177e1e9003a4052a
Kacper Kołodziej <kacper@kolodziej.in>
2015-03-31 20:54:51 +0200

    removed rubbish

--- quicksort.hs
+++
@@ -1,3 +0,0 @@
-quicksort :: (Ord a) => [a] -> [a]
-quicksort [] = []
-quicksort (x:xs) = (quicksort [a | a <- xs, a <= x]) ++ [x] ++ (quicksort [a | a <- xs, a > x])
--- trojkat_pascala.hs
+++
@@ -1,8 +0,0 @@
-silnia :: Integer -> Integer
-silnia 0 = 1
-silnia 1 = 1
-silnia x = x * silnia (x - 1)
-
-trojkatPascala :: Integer -> Integer -> Integer
-trojkatPascala _ 0 = 1
-trojkatPascala r c = silnia r `div` (silnia c * silnia (r-c))
